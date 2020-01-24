bf1627a55019e924b5c703de690422f392e38822
Andrey Mayskiy <andrey.mayskiy@ingrammicro.com>
2020-01-24 17:05:35 +0300

    upload bot release 1.0

---
+++ config.py
@@ -0,0 +1,132 @@
+from telebot import types
+from dyct import d_hydro, d_drugs, d_leaders
+
+token = '1028529844:AAGa946n3V_L4RP4kYio2IvJlIbh_OWHxZQ'
+
+def create_menu(menu):
+
+# Функция создания главного меню
+
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	markup.row(menu[0], menu[1], menu[2])
+	markup.row('Главное Меню')
+
+	return markup
+
+def create_menu_leaders(menu):
+
+# Функция создания меню лидеров фракций
+
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	markup.row(menu[0], menu[1])
+	markup.row(menu[2], menu[3])
+	markup.row('Главное Меню')
+
+	return markup
+
+def create_menu_tactics(menu):
+
+# Функция создания меню тактических карт
+
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+	if len(menu) == 6:
+		cut = menu[:6]
+		markup.row(cut[0], cut[1], cut[2])
+		markup.row(cut[3], cut[4], cut[5])
+		del menu[:6]	
+	else:
+		print('Error while creating menu of tactics')
+
+	markup.row('Назад - Лидеры')
+
+	return markup
+
+def create_menu_detail(menu):
+
+# Функция создания меню для юнитов фракций
+
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+	if len(menu) == 6:
+		cut = menu[:6]
+		markup.row(cut[0], cut[1], cut[2])
+		markup.row(cut[3], cut[4], cut[5])
+		del menu[:6]
+
+	elif len(menu) == 9:
+		cut = menu[:9]
+		markup.row(cut[0], cut[1], cut[2])
+		markup.row(cut[3], cut[4], cut[5])
+		markup.row(cut[6], cut[7], cut[8])
+		del menu[:9]
+
+	elif len(menu) == 10:
+		cut = menu[:10]
+		markup.row(cut[0], cut[1])
+		markup.row(cut[2], cut[3], cut[4])
+		markup.row(cut[5], cut[6], cut[7])
+		markup.row(cut[8], cut[9])
+		del menu[:10]
+
+	elif len(menu) == 13:
+		cut = menu[:13]
+		markup.row(cut[0], cut[1], cut[2])
+		markup.row(cut[3], cut[4], cut[5])
+		markup.row(cut[6], cut[7], cut[8])
+		markup.row(cut[9], cut[10])
+		markup.row(cut[11], cut[12])
+		del menu[:13]		
+
+	markup.row('Назад - Фракции')
+
+	return markup
+
+class items:
+	
+	def __init__(self, bot, message):
+		
+		self.bot = bot
+		self.message = message
+
+	def get_units(self):
+
+	# Функция создания меню персонажей из вызова по ключу фракции
+
+		user_id = self.message.from_user.id
+		menu = list(d_hydro[self.message.text])
+		markup = create_menu_detail(menu)
+		self.bot.send_message(user_id, 'Выбери персонажа:', reply_markup=markup)
+
+	def get_unit_card(self, frac):
+
+	# Функция получения карточки юнита из вызова по ключу фракции и персонажа
+		
+		user_id = self.message.from_user.id
+		url = str(d_hydro[frac][self.message.text])
+		self.bot.send_photo(user_id, url)
+
+	def get_drug_card(self):
+
+	# Функция получения карт усилителей по ключу
+
+		user_id = self.message.from_user.id
+		url = str(d_drugs[self.message.text])
+		self.bot.send_photo(user_id, url)
+
+	def get_leaders(self):
+
+	# Функция создания меню тактический карт из вызова по ключу лидера
+
+		user_id = self.message.from_user.id
+		menu = list(d_leaders[self.message.text])
+		markup = create_menu_tactics(menu)
+		self.bot.send_message(user_id, 'Выбери карточку:', reply_markup=markup)	
+
+	def get_leaders_card(self, card):	
+
+	# Функция получения карточки лидера из вызова по ключу лидера и его карточки
+
+		user_id = self.message.from_user.id
+		url = str(d_leaders[card][self.message.text])
+		self.bot.send_photo(user_id, url)
---
+++ dyct.py
@@ -0,0 +1,110 @@
+
+# словарь фракций
+d_hydro = {
+	'Правительство': {
+		'Дэвис': 'https://imgur.com/AMakxAk',
+		'Фемида': 'https://imgur.com/NaO8Xu5',
+		'Охотник': 'https://imgur.com/Ut9KoQu',
+		'Снайпер': 'https://imgur.com/ITGda9s',
+		'Инспектор': 'https://imgur.com/kARxrT6',
+		'Патрульный': 'https://imgur.com/66GDxBr'
+	},
+	'Синдикат': {
+		'Винсент': 'https://imgur.com/sgcUAQE',
+		'Заводила': 'https://imgur.com/zbMwsBi',
+		'Стефан': 'https://imgur.com/zNNx9cJ',
+		'Тень': 'https://imgur.com/ZfJj8X4',
+		'Гидроид': 'https://imgur.com/ipgIIvC',
+		'Чарли': 'https://imgur.com/DkNkKuo',
+		'Хакер': 'https://imgur.com/SIjjjXm',
+		'Дилер': 'https://imgur.com/gEpWvmC',
+		'Панк': 'https://imgur.com/67jy5Jf',
+		'Повстанец': 'https://imgur.com/sKsD3G7'
+	},
+	'Нисимура': {
+		'Тэцуи': 'https://imgur.com/M1wMeVC',
+		'Техник': 'https://imgur.com/JjpG9Pl',
+		'Контроллер':'https://imgur.com/8PBpWYz',
+		'Бегемот I': 'https://imgur.com/39kwWh2',
+		'Сольпуга I': 'https://imgur.com/4ZOZON7',
+		'Шакал I': 'https://imgur.com/EU4tUMz',
+		'Бегемот II':'https://imgur.com/bn5shfp',
+		'Сольпуга II': 'https://imgur.com/DJscPjO',
+		'Шакал II': 'https://imgur.com/POslpaK',
+		'Жук I': 'https://imgur.com/gNkPbUl',
+		'Жук II': 'https://imgur.com/S4w56MQ',
+		'Стрекоза I': 'https://imgur.com/LPYHKs5',
+		'Стрекоза II': 'https://imgur.com/VdBUugl'
+		}
+}
+
+
+# словарь миссий
+d_mis = {
+	'url_mis_down': 'https://imgur.com/FhBRQiv',
+	'url_mis_grab': 'https://imgur.com/OVmrJDN',
+	'url_mis_inv': 'https://imgur.com/wabdjWh',
+	'url_mis_hold': 'https://imgur.com/IO0wHLH'
+}
+
+
+# словарь расстановок
+d_set = {
+	'url_set_clas': 'https://imgur.com/hWYrVeU',
+	'url_set_kl': 'https://imgur.com/gNxKqKW',
+	'url_set_fl': 'https://imgur.com/Y6chgCJ',
+	'url_set_fl_r': 'https://imgur.com/BmyrPsr',
+	'url_set_fl_l': 'https://imgur.com/TvFh6Oj',
+	'url_set_kl': 'https://imgur.com/gNxKqKW'
+}
+
+
+# словарь лидеров
+d_leaders = {
+	'Правительство - Дэвис': {
+		'Усмирение': 'https://imgur.com/BF5y7WL',
+		'Старый солдат': 'https://imgur.com/mcN9MvI',
+		'Железный кэп': 'https://imgur.com/hI07iXJ',
+		'Рейд': 'https://imgur.com/hJrhd45',
+		'Досмотр': 'https://imgur.com/jctIul8',
+		'На страже закона': 'https://imgur.com/UTjlV0U'
+	},
+	'Синдикат - Винсент': {
+		'Скрытая угроза': 'https://imgur.com/RHWoEG0',
+		'Проводник': 'https://imgur.com/Kk8N2Ui',
+		'Поиски правды': 'https://imgur.com/lFkh5je',
+		'Раненый зверь': 'https://imgur.com/hQnwgLT',
+		'Еще один "Джимми"': 'https://imgur.com/naQEOvv',
+		'Sentiment': 'https://imgur.com/Q0hqSSJ'
+	},
+	'Синдикат - Заводила': {
+		'Митинг': 'https://imgur.com/xal8gdu',
+		'Толкач': 'https://imgur.com/ynIwq1d',
+		'Хард рок': 'https://imgur.com/ypMbT0D',
+		'Мой район': 'https://imgur.com/3Gdcz7j',
+		'Рэйв': 'https://imgur.com/z9162Eh',
+		'Заговорщик': 'https://imgur.com/KDwtqp2'
+	},
+	'Нисимура - Тэцуи': {
+		'Кумир': 'https://imgur.com/u1oSsWO',
+		'Устранитель': 'https://imgur.com/EopIMWN',
+		'Каток': 'https://imgur.com/M40L1NQ',
+		'Защита данных': 'https://imgur.com/2snlIgT',
+		'Мизантроп': 'https://imgur.com/S9kMac0',
+		'Мать роя': 'https://imgur.com/2lTN2BL'
+	}
+}
+
+
+# словарь усилителей
+d_drugs = {
+	'Лазарь': 'https://imgur.com/3tw6F9J',
+	'Швея': 'https://imgur.com/1VJDQGC',
+	'Засвет': 'https://imgur.com/MzgnRIM',
+	'Турбо': 'https://imgur.com/gID6Xjb',
+	'Зонт': 'https://imgur.com/JQNOswF',
+	'Кошкодер': 'https://imgur.com/ALAjfdE',
+	'Циклон': 'https://imgur.com/K5qNaee',
+	'Психоз': 'https://imgur.com/VDmdMrY',
+	'Зип': 'https://imgur.com/Tu7rCX2'
+}
