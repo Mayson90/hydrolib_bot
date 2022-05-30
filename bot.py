3ad71543f76c19dc7b735a3cd8810d37616ad372
Andrey Mayskiy <andrey.mayskiy@ingrammicro.com>
2022-05-30 14:12:36 +0300

    add 'Скачать' row, add 'FAQ' and move all downloads to 'Скачать', add new class in func.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
-import telebot
 import random
+
+import telebot
+from telebot import types
+
 import config
 import db_users
-
-from telebot import types
-from func import create_menu, create_main_menu, create_menu_detail, items
 from dyct import d_hydro, d_mis, d_set, d_drugs, d_leaders
-
+from func import create_menu, create_main_menu, GetCards
 
 bot = telebot.TeleBot(config.token)
 
@@ -14,29 +14,33 @@ text_messages = {
     'start':
         u'Приветствую тебя, {name}!\n\n'
         u'Навигация по ГЛАВНОМУ МЕНЮ:\n\n'
-        u'|ФРАКЦИИ| - навигация по перснонажам фракций\n'
+        u'|ФРАКЦИИ| - навигация по персонажам фракций\n'
         u'|УСИЛИТЕЛИ| - получить карты усилителей\n'
         u'|ТАКТИКИ| - получить карты тактик\n'
         u'|ПРАВИЛА| - скачать актуальные правила\n'
+        u'|FAQ| - пояснения к карточкам по конкретным моделям для каждой фракции\n'
         u'|РАНДОМАЙЗЕР| - случайный выбор миссии и расстановки\n\n'
         u'кнопка |Главное Меню| снизу',
-    
+
     'help':
         u'С предложениям по улучшению, а также об ошибках\n'
-        u'просьба писать - https://vk.com/33ud934nf\n',
+        u'просьба писать - https://discord.gg/eaWJFDdddM\n',
 
     'destroy':
         u'011011100110111101110111001000000110100100100000011000010110110100100000011000100110010101100011011011110110110101100101001000000110010001100101011000010111010001101000001000000111010001101000011001010010000001100100011001010111001101110100011100100110111101111001011001010111001000100000011011110110011000100000011101110110111101110010011011000110010001110011\n'
 }
 
-# Меню основных комманд бота
+
+# Меню основных команд бота
 
 @bot.message_handler(commands=['start'])
 def get_start(message):
     db_users.create_user(message)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.row('Главное Меню')
-    bot.send_message(message.from_user.id, text_messages['start'].format(name=message.from_user.first_name), reply_markup=markup)
+    bot.send_message(message.from_user.id, text_messages['start'].format(name=message.from_user.first_name),
+                     reply_markup=markup)
+
 
 @bot.message_handler(commands=['help'])
 def get_start(message):
@@ -45,6 +49,7 @@ def get_start(message):
     markup.row('Главное Меню')
     bot.send_message(message.from_user.id, text_messages['help'], reply_markup=markup)
 
+
 @bot.message_handler(commands=['destroy'])
 def get_start(message):
     db_users.create_user(message)
@@ -52,206 +57,230 @@ def get_start(message):
     bot.send_message(message.from_user.id, text_messages['destroy'])
     bot.send_animation(message.from_user.id, animation)
 
+
 @bot.message_handler(content_types=['text'])
 def get_nav(message):
     db_users.create_user(message)
 
-# Функция навигации по фракциям и персонажам
+    # Функция навигации по фракциям и персонажам
 
     if message.text == 'Главное Меню':
         get_menu(message)
 
     if message.text == 'Фракции':
         get_fractions(message)
-		
+
     elif message.text == 'Усилители':
         get_drugs(message)
 
     elif message.text == 'Тактики':
         get_tactics(message)
 
+    elif message.text == 'Скачать':
+        get_downloads(message)
+
     elif message.text == 'Правила':
         get_rules(message)
 
     elif message.text == 'Гидропедия':
-        get_pedia(message)    
+        get_pedia(message)
+
+    elif message.text == 'FAQ':
+        get_faq(message)
 
     elif message.text == 'Рандомайзер':
-        get_random(message)	
+        get_random(message)
 
     elif message.text == 'назад - Лидеры':
-        get_tactics(message)	
+        get_tactics(message)
 
     elif message.text == 'назад - Фракции':
-        get_fractions(message)		
+        get_fractions(message)
 
     elif message.text in list(d_drugs):
-    # Получить список усилителей
+        # Получить список усилителей
         get_drugs_all(message)
 
     elif message.text in list(d_leaders):
-    # Получить список лидеров фракций
+        # Получить список лидеров фракций
         get_all_leaders(message)
 
     elif message.text in list(d_leaders['Правительство - Дэвис']):
-    # Получить карточки Дэвиса
-        get_devis(message)		
+        # Получить карточки Дэвиса
+        get_devis(message)
 
     elif message.text in list(d_leaders['Синдикат - Винсент']):
-    # Получить карточки Винсента
-        get_vinsent(message)	
+        # Получить карточки Винсента
+        get_vinsent(message)
 
     elif message.text in list(d_leaders['Синдикат - Заводила']):
-    # Получить карточки Заводилы
+        # Получить карточки Заводилы
         get_firebrand(message)
 
     elif message.text in list(d_leaders['Нисимура - Тэцуи']):
-    # Получить карточки Тэцуи
-        get_tetsui(message)	
+        # Получить карточки Тэцуи
+        get_tetsui(message)
 
     elif message.text in list(d_hydro):
-    # Получить юнитов фракции
-        get_all_units(message)			
+        # Получить персонажей фракции
+        get_all_units(message)
 
     elif message.text in list(d_hydro['Правительство']):
-    # Получить карточки юнитов Правительства
+        # Получить карточки персонажей Правительства
         get_gov(message)
-	
+
     elif message.text in list(d_hydro['Синдикат']):
-    # Получить карточки юнитов Синдиката
+        # Получить карточки персонажей Синдиката
         get_syn(message)
 
     elif message.text in list(d_hydro['Нисимура']):
-    # Получить карточки юнитов Нисимуры
+        # Получить карточки персонажей Нисимуры
         get_nis(message)
 
 
 def get_menu(message):
-
-# Функция главного меню
+    # Функция главного меню
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.row('Фракции', 'Усилители', 'Тактики')
-    markup.row('Правила', 'Гидропедия', 'Рандомайзер')
+    markup.row('Скачать', 'Рандомайзер')
     bot.send_message(message.from_user.id, 'Выбери категорию:', reply_markup=markup)
 
-def get_rules(message):
 
-# Функция загрузки правил
-	
+def get_downloads(message):
+    # Функция меню загрузок
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row('Правила', 'Гидропедия', 'FAQ')
+    markup.row('Главное Меню')
+    bot.send_message(message.from_user.id, 'Выбери категорию:', reply_markup=markup)
+
+
+def get_rules(message):
+    # Функция загрузки правил
+
     url = 'https://drive.google.com/uc?export=download&id=1O0gneqLQzETQ-Mn8SqsXqe0z7LY9vwIO'
     bot.send_message(message.from_user.id, 'документ загружается...')
     bot.send_document(message.from_user.id, url)
 
-def get_pedia(message):
 
-# Функции загрузки гидропедии
+def get_pedia(message):
+    # Функции загрузки гидропедии
 
     url = 'https://drive.google.com/uc?export=download&id=1WZHJQJ3NEDtm27jb1VMNkBWyT5wp2zZO'
     bot.send_message(message.from_user.id, 'документ загружается...')
     bot.send_document(message.from_user.id, url)
 
-def get_drugs(message):
 
-# Функция навигации по усилителям
+def get_faq(message):
+    # Функции загрузки гидропедии
+
+    url = 'https://drive.google.com/uc?export=download&id=1XAxfrmShesX-pzJdVsKdVCPEFCvNwbnK'
+    bot.send_message(message.from_user.id, 'документ загружается...')
+    bot.send_document(message.from_user.id, url)
+
+
+def get_drugs(message):
+    # Функция навигации по усилителям
 
     menu = list(d_drugs.keys())
     markup = create_main_menu(menu)
     bot.send_message(message.from_user.id, 'Выбери усилитель:', reply_markup=markup)
 
+
 def get_drugs_all(message):
+    # Функция возвращает карты усилителей
 
-# Функция возвращает карты усилителей
-
-    drugs = items(bot, message)
+    drugs = GetCards(bot, message)
     drugs.get_drug_card()
 
-def get_tactics(message):
 
-# Функция навигации по тактикам лидеров фракций
+def get_tactics(message):
+    # Функция навигации по тактикам лидеров фракций
 
     menu = list(d_leaders.keys())
     markup = create_main_menu(menu)
     bot.send_message(message.from_user.id, 'Выбери лидера:', reply_markup=markup)
 
+
 def get_all_leaders(message):
+    # Функция возвращает список всех карт Лидеров
 
-# Функция возвращает список всех карт Лидеров
-
-    l_cards = items(bot, message)
+    l_cards = GetCards(bot, message)
     l_cards.get_leaders()
 
+
 def get_devis(message):
+    # Функция возвращает карты Дэвиса
 
-# Функция возвращает карты Дэвиса
-
-    devis = items(bot, message)
+    devis = GetCards(bot, message)
     devis.get_leaders_card('Правительство - Дэвис')
 
+
 def get_vinsent(message):
+    # Функция возвращает карты Винсента
 
-# Функция возвращает карты Винсента
-
-    vinsent = items(bot, message)
+    vinsent = GetCards(bot, message)
     vinsent.get_leaders_card('Синдикат - Винсент')
 
+
 def get_firebrand(message):
+    # Функция возвращает карты Заводилы
 
-# Функция возвращает карты Заводилы
-
-    firebrand = items(bot, message)
+    firebrand = GetCards(bot, message)
     firebrand.get_leaders_card('Синдикат - Заводила')
 
+
 def get_tetsui(message):
+    # Функция возвращает карты Тэцуи
 
-# Функция возвращает карты Тэцуи
+    tetsui = GetCards(bot, message)
+    tetsui.get_leaders_card('Нисимура - Тэцуи')
 
-    tetsui = items(bot, message)
-    tetsui.get_leaders_card('Нисимура - Тэцуи')			
 
 def get_fractions(message):
-
-# Функция навигации по фракциям
+    # Функция навигации по фракциям
 
     menu = list(d_hydro.keys())
     markup = create_menu(menu)
     bot.send_message(message.from_user.id, 'Выбери фракцию:', reply_markup=markup)
 
-def get_random(message):
 
-# Функция рандомного выбора миссий и расстановок
+def get_random(message):
+    # Функция случайного выбора миссий и расстановок
 
     random_mis = random.choice(list(d_mis.values()))
     random_set = random.choice(list(d_set.values()))
     bot.send_photo(message.from_user.id, random_mis)
     bot.send_photo(message.from_user.id, random_set)
 
+
 def get_all_units(message):
+    # Функция навигации по персонажам всех Фракций
 
-# Функция навигации по персонажам всех Фракций
-
-    units = items(bot, message)
+    units = GetCards(bot, message)
     units.get_units()
 
+
 def get_syn(message):
+    # Функция возвращает карты персонажей Синдиката
 
-# Функция возвращает карты юнитов Синдиката
-
-    syn = items(bot, message)
+    syn = GetCards(bot, message)
     syn.get_unit_card('Синдикат')
 
+
 def get_nis(message):
+    # Функция возвращает карты персонажей Нисимуры
 
-# Функция возвращает карты юнитов Нисимуры
-
-    nis = items(bot, message)
+    nis = GetCards(bot, message)
     nis.get_unit_card('Нисимура')
 
-def get_gov(message):
 
-# Функция возвращает карты юнитов Правительства
-	
-    gov = items(bot, message)
+def get_gov(message):
+    # Функция возвращает карты персонажей Правительства
+
+    gov = GetCards(bot, message)
     gov.get_unit_card('Правительство')
+
 
 bot.polling(none_stop=True)
--- func.py
+++ func.py
@@ -1,10 +1,10 @@
 from telebot import types
+
 from dyct import d_hydro, d_drugs, d_leaders
 
 
 def create_menu(menu):
-
-# Функция создания главного меню
+    # Функция создания главного меню
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.row(menu[0], menu[1], menu[2])
@@ -12,9 +12,9 @@ def create_menu(menu):
 
     return markup
 
-def create_main_menu(menu):
 
-# Функция создания меню c возвратом в главное меню
+def create_main_menu(menu):
+    # Функция создания меню c возвратом в главное меню
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
@@ -35,9 +35,9 @@ def create_main_menu(menu):
 
     return markup
 
-def create_menu_tactics(menu):
 
-# Функция создания меню тактических карт
+def create_menu_tactics(menu):
+    # Функция создания меню тактических карт
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
@@ -45,7 +45,7 @@ def create_menu_tactics(menu):
         cut = menu[:6]
         markup.row(cut[0], cut[1], cut[2])
         markup.row(cut[3], cut[4], cut[5])
-        del menu[:6]	
+        del menu[:6]
     else:
         print('Error while creating menu of tactics')
 
@@ -53,9 +53,9 @@ def create_menu_tactics(menu):
 
     return markup
 
-def create_menu_detail(menu):
 
-# Функция создания меню для юнитов фракций
+def create_menu_detail(menu):
+    # Функция создания меню для юнитов фракций
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
@@ -82,22 +82,21 @@ def create_menu_detail(menu):
         markup.row(cut[9], cut[10])
         markup.row(cut[11], cut[12])
         markup.row(cut[13])
-        del menu[:14]		
+        del menu[:14]
 
     markup.row('назад - Фракции')
 
     return markup
 
-class items:
-	
+
+class GetCards:
+
     def __init__(self, bot, message):
-		
         self.bot = bot
         self.message = message
 
     def get_units(self):
-
-    # Функция создания меню персонажей из вызова по ключу фракции
+        # Функция создания меню персонажей из вызова по ключу фракции
 
         user_id = self.message.from_user.id
         menu = list(d_hydro[self.message.text])
@@ -105,33 +104,29 @@ class items:
         self.bot.send_message(user_id, 'Выбери персонажа:', reply_markup=markup)
 
     def get_unit_card(self, frac):
+        # Функция получения карточки персонажа из вызова по ключу фракции и персонажа
 
-    # Функция получения карточки юнита из вызова по ключу фракции и персонажа
-		
         user_id = self.message.from_user.id
         url = str(d_hydro[frac][self.message.text])
         self.bot.send_photo(user_id, url)
 
     def get_drug_card(self):
-
-    # Функция получения карт усилителей по ключу
+        # Функция получения карт усилителей по ключу
 
         user_id = self.message.from_user.id
         url = str(d_drugs[self.message.text])
         self.bot.send_photo(user_id, url)
 
     def get_leaders(self):
-
-    # Функция создания меню тактический карт из вызова по ключу лидера
+        # Функция создания меню тактический карт из вызова по ключу лидера
 
         user_id = self.message.from_user.id
         menu = list(d_leaders[self.message.text])
         markup = create_menu_tactics(menu)
-        self.bot.send_message(user_id, 'Выбери карточку:', reply_markup=markup)	
+        self.bot.send_message(user_id, 'Выбери карточку:', reply_markup=markup)
 
-    def get_leaders_card(self, card):	
-
-    # Функция получения карточки лидера из вызова по ключу лидера и его карточки
+    def get_leaders_card(self, card):
+        # Функция получения карточки лидера из вызова по ключу лидера и его карточки
 
         user_id = self.message.from_user.id
         url = str(d_leaders[card][self.message.text])
